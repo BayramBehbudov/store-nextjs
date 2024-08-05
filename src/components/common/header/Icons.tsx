09beb383b1f1d2b266a1b97da3365ac9bcb4f219
BayramBehbudov <bayram.behbudov@gmail.com>
2024-08-05 19:12:31 +0400

    update header

--- src/components/common/header/Icons.tsx
+++ src/components/common/header/Icons.tsx
@@ -6,16 +6,10 @@ interface IIcons {}
 
 const Icons: React.FC = (): JSX.Element => {
    return (
-      <div className="flex h-full items-center gap-5">
-         <FaHeart size={35} className="cursor-pointer text-[#60DA00] sm:text-[#720498]" />
-         <PiShoppingBagOpenFill size={35} className="cursor-pointer text-[#60DA00] sm:text-[#720498]" />
-         <div className="flex h-full w-fit cursor-pointer items-center gap-2">
-            <FaUserLarge size={35} className="cursor-pointer text-[#60DA00] sm:text-[#720498]" />
-            <div className="sm:hidden">
-               <p className="text-[14px] font-bold text-[#720498]">First Name</p>
-               <p className="text-[14px] font-bold text-[#60DA00]">Last Name</p>
-            </div>
-         </div>
+      <div className="flex h-full items-center gap-5 pr-3">
+         <FaHeart size={35} className="cursor-pointer text-[#60DA00] md:text-[#720498]" />
+         <PiShoppingBagOpenFill size={35} className="cursor-pointer text-[#60DA00] md:text-[#720498]" />
+         <FaUserLarge size={35} className="cursor-pointer text-[#60DA00] md:text-[#720498]" />
       </div>
    );
 };
